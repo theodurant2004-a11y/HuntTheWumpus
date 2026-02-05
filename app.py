04f525307a453361f4d369dad885ff1b8232a1e5
Theo <theodurant2004@gmail.com>
2026-02-05 20:42:26 +0100

    Nettoyage et structure final

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")# quand on se rend à cet url la fonction en dessous s'execute
-def hello_world():# fonction executé au lancement
+def home():# fonction executé au lancement
     return render_template("index.html")# renvoie sur la page index.html
 
 if __name__ == '__main__':
