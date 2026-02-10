a5ed0093f4c80084ab62d0fcd493fa22132d7d99
Augustin Dellisse <augustin.dellisse@gmail.com>
2026-02-10 13:22:53 +0100

    nouveau depart

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")# quand on se rend à cet url la fonction en dessous s'execute
+def home():# fonction executé au lancement
+    return render_template("index.html")# renvoie sur la page index.html
+
+if __name__ == '__main__':
+    app.run(debug=True)
