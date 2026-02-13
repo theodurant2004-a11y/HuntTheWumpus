9ea9c480cf44d11d8c5a58e9ab7a51c4e26357c0
Theo <theodurant2004@gmail.com>
2026-02-13 11:33:54 +0100

    Page podium + lien gameScreen

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template 
 
 app = Flask(__name__)
-@app.route('/')
 
-def Home():
+@app.route('/')
+def login():
     return render_template("index.html")
+
+@app.route('/podiumScreen')
+def podiumScreen():
+    return render_template('podiumScreen.html')
+
+@app.route('/gameScreen')
+def gameScreen():
+    return render_template('gameScreen.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
