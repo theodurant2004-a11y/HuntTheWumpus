e438b9b11b90a789117ea653cafdb5330968c1a6
Augustin Dellisse <augustin.dellisse@gmail.com>
2026-03-02 13:41:34 +0100

    maze variable in session + update .gitignore

--- routes/game.py
+++ routes/game.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, redirect, url_for
 from wumpus_engine import mazeGeneration
 from routes.auth import login_required, get_db_connection
 
@@ -9,12 +9,7 @@ RIGHT = 96
 
 # TMP
 DIFFICULTY = 2
-
-maze = []
-vision_maze = []
-coming_from_hist = []
-bats_maze = []
-####################
+#############
 
 game_bp = Blueprint('game', __name__)
 
@@ -35,48 +30,96 @@ def podiumScreen():
 @game_bp.route('/gameScreen')
 @login_required
 def gameScreen():
-    global vision_maze, coming_from_hist, maze, bats_maze
-
     avatar_du_joueur = session.get('joueur_avatar', '1.png') 
     #c'est au cas ou y a un bug dans la session, on s'en fous de l'avatar donc on en donne un par défault si il en a pas 
 
-    #TODO
-    #mettre en variable session
     maze = mazeGeneration.generateMaze(DIFFICULTY)
     vision_maze = [[0 for _ in range(8)] for _ in range(6)]
     coming_from_hist = []
     bats_maze = mazeGeneration.generateBats(DIFFICULTY)
-    ###########################
 
     spawnpoint = mazeGeneration.starting_point(maze)
     vision_maze[spawnpoint[1]][spawnpoint[0]] = 2
 
+    session['maze'] = maze
+    session['vision_maze'] = vision_maze
+    session['coming_from_hist'] = coming_from_hist
+    session['bats_maze'] = bats_maze
+
     return render_template('gameScreen.html', maze=maze, vision_maze=vision_maze, bats=bats_maze)
 
-@game_bp.route('/move/up')# si fin de partie afficher tout le labyrinthe
+@game_bp.route('/move/up')
 @login_required
 def moveUp():
-    global vision_maze
+    if 'maze' not in session:
+        return redirect(url_for('game.gameScreen'))
+
+    maze = session['maze']
+    vision_maze = session['vision_maze']
+    coming_from_hist = session['coming_from_hist']
+    bats_maze = session['bats_maze']
+
     vision_maze = mazeGeneration.move(maze, vision_maze, TOP, coming_from_hist)
+
+    session['vision_maze'] = vision_maze
+    session['coming_from_hist'] = coming_from_hist
+
+    session.modified = True
     return render_template('gameScreen.html', maze=maze, vision_maze=vision_maze, bats=bats_maze)
 
 @game_bp.route('/move/down')
 @login_required
 def moveDown():
-    global vision_maze
+    if 'maze' not in session:
+        return redirect(url_for('game.gameScreen'))
+    
+    maze = session['maze']
+    vision_maze = session['vision_maze']
+    coming_from_hist = session['coming_from_hist']
+    bats_maze = session['bats_maze']
+
     vision_maze = mazeGeneration.move(maze, vision_maze, BOTTOM, coming_from_hist)
+
+    session['vision_maze'] = vision_maze
+    session['coming_from_hist'] = coming_from_hist
+
+    session.modified = True
     return render_template('gameScreen.html', maze=maze, vision_maze=vision_maze, bats=bats_maze)
 
 @game_bp.route('/move/left')
 @login_required
 def moveLeft():
-    global vision_maze
+    if 'maze' not in session:
+        return redirect(url_for('game.gameScreen'))
+    
+    maze = session['maze']
+    vision_maze = session['vision_maze']
+    coming_from_hist = session['coming_from_hist']
+    bats_maze = session['bats_maze']
+
     vision_maze = mazeGeneration.move(maze, vision_maze, LEFT, coming_from_hist)
+
+    session['vision_maze'] = vision_maze
+    session['coming_from_hist'] = coming_from_hist
+
+    session.modified = True
     return render_template('gameScreen.html', maze=maze, vision_maze=vision_maze, bats=bats_maze)
 
 @game_bp.route('/move/right')
 @login_required
 def moveRight():
-    global vision_maze
+    if 'maze' not in session:
+        return redirect(url_for('game.gameScreen'))
+    
+    maze = session['maze']
+    vision_maze = session['vision_maze']
+    coming_from_hist = session['coming_from_hist']
+    bats_maze = session['bats_maze']
+
     vision_maze = mazeGeneration.move(maze, vision_maze, RIGHT, coming_from_hist)
+
+    session['vision_maze'] = vision_maze
+    session['coming_from_hist'] = coming_from_hist
+
+    session.modified = True
     return render_template('gameScreen.html', maze=maze, vision_maze=vision_maze, bats=bats_maze)
